2916ba78c4854cd8fb58b1645b66201430cf160b
ricky gould <rgould2021@fau.edu>
2023-10-01 22:01:46 -0400

    Step 6: Compose and save Post

--- lab-insta-parse/Models/Post.swift
+++ lab-insta-parse/Models/Post.swift
@@ -12,8 +12,19 @@ import ParseSwift
 
 
 // TODO: Pt 1 - Create Post Parse Object model
+struct Post: ParseObject {
+    // These are required by ParseObject
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+
+    // Your own custom properties.
+    var caption: String?
+    var user: User?
+    var imageFile: ParseFile?
+}
 // https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L33
 
-struct Post {
-    
-}
+
